554abeba65c13f91280648d32019b2be117e808a
m00236484 <eng.2halil@gmail.com>
2019-04-03 21:14:41 -0500

    add dataset class

--- src/services/fdaMngr.py
+++ src/services/fdaMngr.py
@@ -46,7 +46,7 @@ class FdaAdapter():
                 ds = DataSetBl()
                 name = "'fda/"  + str(category) + "/" + str(ds) +"'"
                 store = "'fda/"  + str(category) + "/" + str(ds) +"'"
-
+                print name
                 ds.setDataSet(ds_src_id ,ds_type ,str(name) ,dsUrl,adpter_type_id ,store  ,refesh_frq)
                 for i in  dataset['partitions']:
                     url =  i['file']
