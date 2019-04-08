46bbd365c9c7aa415e8f64134d9ee02a0a748a07
m00236484 <eng.2halil@gmail.com>
2019-04-08 13:30:15 -0500

    add dataset class

--- src/services/ds_download.py
+++
@@ -1,8 +0,0 @@
-import wget
-
-dwnToS3 = 'arn:aws:s3:::ds-temp-dir'
-url = 'https://download.open.fda.gov/device/enforcement/device-enforcement-0001-of-0001.json.zip'
-wget.download(url, -ouput='arn:aws:s3:::ds-temp-dir')
-
-
-
