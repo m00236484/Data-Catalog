d059cd999a76372f31b54c74178297d87a99819c
m00236484 <eng.2halil@gmail.com>
2019-04-10 16:51:06 -0500

    add dataset class

---
+++ src/flaskr/config.py
@@ -0,0 +1,29 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+    SQLALCHEMY_DATABASE_URI = os.environ['postgresql://ec2-34-211-128-184.us-west-2.compute.amazonaws.com/datacatalog']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
---
+++ src/flaskr/flaskr.py
@@ -0,0 +1,34 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask.ext.sqlalchemy import SQLAlchemy
+from models import db
+
+
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'postgres',
+    'db': 'datacatalog',
+    'host': 'ec2-34-211-128-184.us-west-2.compute.amazonaws.com',
+    'port': '5432',
+}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db = SQLAlchemy(app)
+db.init_app(app)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
